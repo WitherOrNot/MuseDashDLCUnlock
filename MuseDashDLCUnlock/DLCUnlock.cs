39461e2b8f1b3b305f8d69a28a875e8e1535f1f0
Wither OrNot <damemem@gmail.com>
2023-10-11 21:50:28 -0400

    Add project files.

---
+++ MuseDashDLCUnlock/DLCUnlock.cs
@@ -0,0 +1,28 @@
+﻿using MuseDashDLCUnlock;
+using MelonLoader;
+using HarmonyLib;
+using Il2CppSteamworks;
+
+[assembly: MelonInfo(typeof(DLCUnlock), "Muse Dash DLC Unlock", "1.0.0", "witherornot")]
+[assembly: MelonGame("PeroPeroGames", "MuseDash")]
+
+namespace MuseDashDLCUnlock
+{
+    [HarmonyPatch(typeof(Il2CppSteamworks.SteamApps), nameof(Il2CppSteamworks.SteamApps.BIsDlcInstalled))]
+    public class DLCPatch
+    {
+        static bool Prefix(ref bool __result, AppId_t appID)
+        {
+            __result = true;
+            return false;
+        }
+    }
+
+    public class DLCUnlock : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Loaded DLC Unlock");
+        }
+    }
+}
