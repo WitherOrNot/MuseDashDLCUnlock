1c5a094f43051e1b3559f0e8f5fd9476d7bb22ae
WitherOrNot <damemem@gmail.com>
2023-11-24 00:46:31 -0500

    Add another patch

--- MuseDashDLCUnlock/DLCUnlock.cs
+++ MuseDashDLCUnlock/DLCUnlock.cs
@@ -2,19 +2,37 @@
 using MelonLoader;
 using HarmonyLib;
 using Il2CppSteamworks;
+using Il2Cpp;
 
-[assembly: MelonInfo(typeof(DLCUnlock), "Muse Dash DLC Unlock", "1.0.0", "witherornot")]
+[assembly: MelonInfo(typeof(DLCUnlock), "Muse Dash DLC Unlock", "1.1.0", "witherornot")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 
 namespace MuseDashDLCUnlock
 {
-    [HarmonyPatch(typeof(Il2CppSteamworks.SteamApps), nameof(Il2CppSteamworks.SteamApps.BIsDlcInstalled))]
+    [HarmonyPatch(typeof(SteamApps), nameof(SteamApps.BIsDlcInstalled))]
     public class DLCPatch
     {
         static bool Prefix(ref bool __result, AppId_t appID)
         {
+#if DEBUG
+            Melon<DLCUnlock>.Logger.Msg($"Called IsDlcInstalled for DLC {appID.m_AppId}");
+#endif
             __result = true;
             return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(SteamManager), nameof(SteamManager.DLCVerify))]
+    public class DLCVerifyPatch
+    {
+        static bool Prefix(SteamManager __instance)
+        {
+#if DEBUG
+            Melon<DLCUnlock>.Logger.Msg("Forcing DLCVerify");
+#endif
+            __instance.m_DoSomething1 = true;
+            __instance.m_DoSomething3 = true;
+            return true;
         }
     }
 
